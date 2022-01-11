3a3d93e15200ce8051c0c9e7c7b6e43bc75edfb6
cas2void <cas2void@gmail.com>
2022-01-11 18:02:00 +0800

    Add testbed project

---
+++ Source/LandscapeTestbed/LandscapeTestbedGameModeBase.h
@@ -0,0 +1,17 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "LandscapeTestbedGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class LANDSCAPETESTBED_API ALandscapeTestbedGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+};

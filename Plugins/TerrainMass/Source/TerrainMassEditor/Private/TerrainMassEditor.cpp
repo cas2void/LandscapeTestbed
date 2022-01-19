6c994c5dd00bd240e5024179d5b3dc1711b3f87d
cas2void <cas2void@gmail.com>
2022-01-19 15:08:23 +0800

    Add plugin TerrainMass

---
+++ Plugins/TerrainMass/Source/TerrainMassEditor/Private/TerrainMassEditor.cpp
@@ -0,0 +1,18 @@
+#include "TerrainMassEditor.h"
+
+#define LOCTEXT_NAMESPACE "FTerrainMassEditorModule"
+
+void FTerrainMassEditorModule::StartupModule()
+{
+	// This code will execute after your module is loaded into memory; the exact timing is specified in the .uplugin file per-module
+}
+
+void FTerrainMassEditorModule::ShutdownModule()
+{
+	// This function may be called during shutdown to clean up your module.  For modules that support dynamic reloading,
+	// we call this function before unloading the module.
+}
+
+#undef LOCTEXT_NAMESPACE
+	
+IMPLEMENT_MODULE(FTerrainMassEditorModule, TerrainMassEditor)

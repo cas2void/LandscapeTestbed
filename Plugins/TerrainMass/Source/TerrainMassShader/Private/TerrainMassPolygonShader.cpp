90a942d4a13451a05f4acdcfa8d0295656c932a0
cas2void <cas2void@gmail.com>
2022-01-24 11:34:58 +0800

    Custom vertex buffer in Polygon shader

--- Plugins/TerrainMass/Source/TerrainMassShader/Private/TerrainMassPolygonShader.cpp
+++ Plugins/TerrainMass/Source/TerrainMassShader/Private/TerrainMassPolygonShader.cpp
@@ -61,8 +61,74 @@ private:
 
 IMPLEMENT_GLOBAL_SHADER(FTerrainMassPolygonShaderPS, "/TerrainMassShaders/TerrainMassPolygon.usf", "MainPS", SF_Pixel);
 
+struct FTerrainMassPolygonVertex
+{
+public:
+    FVector4 Position;
+    FVector2D UV;
+};
+
+class FTerrainMassPolygonVertexDeclaration : public FRenderResource
+{
+public:
+	FVertexDeclarationRHIRef VertexDeclarationRHI;
+
+	/** Destructor. */
+	virtual ~FTerrainMassPolygonVertexDeclaration() {}
+
+	virtual void InitRHI()
+	{
+		FVertexDeclarationElementList Elements;
+		uint32 Stride = sizeof(FTerrainMassPolygonVertex);
+		Elements.Add(FVertexElement(0, STRUCT_OFFSET(FTerrainMassPolygonVertex, Position), VET_Float4, 0, Stride));
+		Elements.Add(FVertexElement(0, STRUCT_OFFSET(FTerrainMassPolygonVertex, UV), VET_Float2, 1, Stride));
+		VertexDeclarationRHI = PipelineStateCache::GetOrCreateVertexDeclaration(Elements);
+	}
+
+	virtual void ReleaseRHI()
+	{
+		VertexDeclarationRHI.SafeRelease();
+	}
+};
+
+TGlobalResource<FTerrainMassPolygonVertexDeclaration> GTerrainMassPolygonVertexDeclaration;
+
 void FTerrainMassPolygonShader::Render(FRHICommandListImmediate& RHICmdList, FRHITexture* SourceTexture, FRHITexture* DestTexture, const FIntPoint& Size, const FTerrainMassPolygonShaderParameter& ShaderParams)
 {
+    //
+    // Vertex Buffer
+    //
+    FVertexBufferRHIRef VertexBufferRHI;
+
+    TResourceArray<FTerrainMassPolygonVertex, VERTEXBUFFER_ALIGNMENT> Vertices;
+    Vertices.SetNumUninitialized(6);
+    
+    Vertices[0].Position = FVector4(1, 1, 0, 1);
+    Vertices[0].UV = FVector2D(1, 1);
+
+    Vertices[1].Position = FVector4(0, 1, 0, 1);
+    Vertices[1].UV = FVector2D(0, 1);
+
+    Vertices[2].Position = FVector4(1, 0, 0, 1);
+    Vertices[2].UV = FVector2D(1, 0);
+
+    Vertices[3].Position = FVector4(0, 0, 0, 1);
+    Vertices[3].UV = FVector2D(0, 0);
+
+    Vertices[4].Position = FVector4(0, 1, 0, 1);
+    Vertices[4].UV = FVector2D(0, 1);
+
+    Vertices[5].Position = FVector4(1, 0, 0, 1);
+    Vertices[5].UV = FVector2D(1, 0);
+
+    // Create vertex buffer. Fill buffer with initial data upon creation
+    FRHIResourceCreateInfo CreateInfo(&Vertices);
+    VertexBufferRHI = RHICreateVertexBuffer(Vertices.GetResourceDataSize(), BUF_Static, CreateInfo);
+
+    //
+    // Render
+    //
+
     IRendererModule* RendererModule = &FModuleManager::GetModuleChecked<IRendererModule>("Renderer");
     FRHIRenderPassInfo RPInfo(DestTexture, ERenderTargetActions::Load_Store);
     RHICmdList.BeginRenderPass(RPInfo, TEXT("TerrainMass"));
@@ -76,7 +142,7 @@ void FTerrainMassPolygonShader::Render(FRHICommandListImmediate& RHICmdList, FRH
         GraphicsPSOInit.DepthStencilState = TStaticDepthStencilState<false, CF_Always>::GetRHI();
 
         FGlobalShaderMap* ShaderMap = GetGlobalShaderMap(GMaxRHIFeatureLevel);
-        TShaderMapRef<FScreenVS> VertexShader(ShaderMap);
+        TShaderMapRef<FTerrainMassPolygonShaderVS> VertexShader(ShaderMap);
         TShaderMapRef<FTerrainMassPolygonShaderPS> PixelShader(ShaderMap);
         PixelShader->SetParameters(RHICmdList, ShaderParams);
 
@@ -87,15 +153,10 @@ void FTerrainMassPolygonShader::Render(FRHICommandListImmediate& RHICmdList, FRH
 
         SetGraphicsPipelineState(RHICmdList, GraphicsPSOInit);
 
-        RendererModule->DrawRectangle(
-            RHICmdList,
-            0, 0,             // Dest X, Y
-            Size.X, Size.Y,   // Dest Width, Height
-            0, 0,             // Source U, V
-            1, 1,             // Source USize, VSize
-            Size,             // Target buffer size
-            FIntPoint(1, 1),  // Source texture size
-            VertexShader);
+        RHICmdList.SetStreamSource(0, VertexBufferRHI, 0);
+        RHICmdList.DrawPrimitive(0, 2, 1);
     }
     RHICmdList.EndRenderPass();
+
+    VertexBufferRHI.SafeRelease();
 }
